84ca23e9b523e89d80bce1b2ca89d7bbb89dde45
Aswin Seshadri <aswinseshadri.k@esab.co.in>
2021-07-08 14:39:02 +0530

    Added Swap

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
+void swap (int* a, int*b)
+{
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+    return;
+}
+
+
 int main(void)
 {
     printf("Hello World"\n );
